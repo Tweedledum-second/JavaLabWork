262f6e4f2bc27fc67e3ea7b4499220b89c4c18e9
ShiDaAl <158536962+ShiDaAl@users.noreply.github.com>
2024-10-15 12:48:14 +0900

    Add files via upload

--- DeckOfCards/src/main/java/com/mycompany/deckofcards/Value.java
+++ DeckOfCards/src/main/java/com/mycompany/deckofcards/Value.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
 package com.mycompany.deckofcards;
 
-/**
- *
- * @author student
- */
 public enum Value {
+    A,
+    V,
+    Q,
+    K,
+    T;
+    
     
 }
