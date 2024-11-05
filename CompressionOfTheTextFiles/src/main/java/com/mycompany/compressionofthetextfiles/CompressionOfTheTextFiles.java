ae7eee0e33f13e51ccfee0782380970f2e25efee
ShiDaAl <158536962+ShiDaAl@users.noreply.github.com>
2024-11-05 12:51:03 +0900

    Add files via upload

--- CompressionOfTheTextFiles/src/main/java/com/mycompany/compressionofthetextfiles/CompressionOfTheTextFiles.java
+++ CompressionOfTheTextFiles/src/main/java/com/mycompany/compressionofthetextfiles/CompressionOfTheTextFiles.java
@@ -3,6 +3,7 @@ package com.mycompany.compressionofthetextfiles;
 public class CompressionOfTheTextFiles {
 
     public static void main(String[] args) {
-        
+        FileCompressor compressor = new FileCompressor();
+        compressor.compressFile("C://Users//student", "3smoda.txt");
     }
 }
--- CompressionOfTheTextFiles/src/main/java/com/mycompany/compressionofthetextfiles/FileCompressor.java
+++ CompressionOfTheTextFiles/src/main/java/com/mycompany/compressionofthetextfiles/FileCompressor.java
@@ -1,13 +1,32 @@
 package com.mycompany.compressionofthetextfiles;
 
 public class FileCompressor {
-    public java.io.File compressFile(){
-        java.io.File file = new java.io.File("C://Пользователи//student//Desktop//" , "3smoda.txt");
+    public java.io.File compressFile(String diractory, String fileName) {
+        java.io.File file = new java.io.File(diractory, fileName);
+        java.util.ArrayList<String> list = new java.util.ArrayList<>();
+        int lineCounter = 0;
         try {
         java.io.FileReader reader = new java.io.FileReader(file);
-        reader.
+        java.io.FileWriter writer = new java.io.FileWriter(file, false);
+        java.util.Scanner scanner = new java.util.Scanner(reader);
+        
+        while(scanner.hasNextLine()) {
+            list.add(scanner.nextLine());
+        }
+        
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i).equals(list.get(i+1))){
+                lineCounter++;
+            } else {
+                writer.write(list.get(i) + " " + lineCounter);
+                lineCounter = 0;
+            }
+        }
+        
         } catch (java.io.FileNotFoundException ex) {
-            
+            System.out.println("Файл не существует");
+        } catch (java.io.IOException ex) {
+            System.out.println("Проблемы с вводом");
         }
         return file;
     }
