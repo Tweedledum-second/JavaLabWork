bf2f0fc3400ce5b91687dacd805a3166b6e20beb
ShiDaAl <158536962+ShiDaAl@users.noreply.github.com>
2024-11-25 11:01:08 +0900

    Add files via upload

--- CompoundInterest/src/main/java/com/mycompany/compoundinterest/CompoundInterestCalculation.java
+++ CompoundInterest/src/main/java/com/mycompany/compoundinterest/CompoundInterestCalculation.java
@@ -4,22 +4,33 @@ import java.math.*;
 
 public class CompoundInterestCalculation {
     public String calculateCompoundInterest(String expression) {
-        int arraySize = 0;
-        double number = 0;
-        arraySize = expression.toCharArray().length;
-        char[] charArray = new char[arraySize];
+        char[] charArray = new char[expression.toCharArray().length];
+        int percentIndex = -1;
+        int commaIndex = -1;
+        int equalIndex = -1;
+        BigDecimal number = new BigDecimal(0);
+        
+        //вывод массива символов, полученного из строки
         charArray = expression.toCharArray();
         for (char runner : charArray) {
             System.out.println(runner);
         }
-        //нужно сделать проверку на наличие в строке ввода нецелых значений
-        for (int i = 0; i < expression.indexOf('%'); i++) {
-                number = charArray[i] * Math.pow(10, expression.indexOf('%') - i);
+        
+        percentIndex = searchForChar(charArray, '%', 0);
+        commaIndex = searchForChar(charArray, ',', 0);
+        equalIndex = searchForChar(charArray, '=', 0);
+        
+        for (;;) {
+            number *= ;
         }
+        
+        //возвращает результат
         String result = new String();
         return result;
     }
     
+    //должно быть редактирование строки ввода такое, чтобы при нажатии на 
+    //Enter в строке ввода после = появлялся ответ
     private char[] editString(char[] charArray) {
         int pointIndex = -1;
         BigDecimal number = new BigDecimal(0);
@@ -30,7 +41,10 @@ public class CompoundInterestCalculation {
         return charArray;
     }
     
-    private BigDecimal calculateNumber(BigDecimal number,char[] digit, int currentIndex, int pointIndex) {
+    //Сборка числа для последующего вычисления процента
+    private BigDecimal calculateNumber(char[] digit, int currentIndex, int pointIndex) {
+        BigDecimal number = new BigDecimal(0);
+        
         for (int i = 0; i <= currentIndex ; i++) {
             if(i != pointIndex) {
                 number.add(new BigDecimal(digit[i] * Math.pow(10, pointIndex - i))); //получение заданного числа
@@ -39,7 +53,14 @@ public class CompoundInterestCalculation {
         return number;
     }
     
+    private int calculatePeriods(char[] digit, int currentIndex) {
+        int periodsNumber = 0;
+        
+        for (int i = 0; i < currentIndex; i++) {
+            periodsNumber += Math.pow()
+    }
     
+    //Поиск символа в массиве
     private int searchForChar(char[] charArray, char ch, int beginIndex) {
         int chIndex = 0;
         for (chIndex = beginIndex; charArray[chIndex] != ch; chIndex++) {
